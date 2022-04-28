06dc7d32028ba67d27702888dc52fb4960481521
Michael Fuchs <michael@derfuchs.net>
2022-04-28 13:00:35 +0200

    Refactored summary interval setting

--- src/components/Settings/SummaryInterval.vue
+++ src/components/Settings/SummaryInterval.vue
@@ -2,19 +2,7 @@
 	<q-card flat>
 
 		<q-card-section>
-			<div class="row items-center no-wrap">
-					<div class="col">
-							<div class="text-primary text-h6">{{ $t('Summaries') }}</div>
-					</div>
-					<transition appear enter-active-class="animated flipInY" leave-active-class="animated flipOutY" :duration="2000">
-						<div class="col-auto" v-show="savingDone">
-							<span class="text-body">
-								saved
-								<q-icon class="q-ml-sm" name="fa-light fa-cloud-check" size="sm" color="positive" />
-							</span>
-						</div>
-					</transition>
-			</div>
+			<div class="text-primary text-h6">{{ $t('Summaries') }}</div>
 			<div class="text q-mt-sm">{{ $t('How often do you want Dobby to give you a status update?') }}</div>
 		</q-card-section>
 
@@ -35,24 +23,13 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { mapGetters } from 'vuex'
+import { defineComponent } from 'vue'
 
-/*
-const NONE = 'none';
-	const DAILY_ONCE = 'daily';
-	const DAILY_TWICE = 'daily_2x';
-	const DAILY_THRICE = 'daily_3x';
-	const HOURLY = 'hourly';
-
-
-*/
 export default defineComponent({
 	name: 'SummaryIntervalSetting',
 	data() {
 		return {
 			settingName: 'summaryInterval',
-			currentSetting: null,
 			availableOptions: [
 				{
 					value: 'none',
@@ -60,7 +37,7 @@ export default defineComponent({
 				},
 				{
 					value: 'daily',
-					label: '1x daily: At the morning',
+					label: '1x daily: In the morning',
 				},
 				{
 					value: 'daily_2x',
@@ -68,7 +45,7 @@ export default defineComponent({
 				},
 				{
 					value: 'daily_3x',
-					label: '3x daily: In the morning, during lunch time and in the evening',
+					label: '3x daily: Morning, lunch time, evening',
 				},
 				{
 					value: 'hourly',
@@ -80,38 +57,15 @@ export default defineComponent({
 			savingDone: false,
 		}
 	},
-	created() {
-		this.currentSetting = this.settingValue(this.settingName)
-	},
-	watch: {
-		currentSetting(newSetting) {
-
-			// Don't dispatch when setting a value first because this is the initial setting
-			if (this.firstSet) {
-				this.firstSet = false
-				return
-			}
-
-			this.saving = true
-			this.$store
-				.dispatch('settings/setToAccount', { key: this.settingName, value: newSetting })
-				.then(() => {
-					this.saving = false
-					this.savingDone = true
-				})
-		},
-		savingDone(newVal) {
-			if (newVal == true) {
-				setTimeout(() => {
-					this.savingDone = false
-				}, 2000)
-			}
-		},
-	},
 	computed: {
-		...mapGetters({
-			settingValue: 'settings/value',
-		}),
+		currentSetting: {
+			get: function() {
+				return this.$store.getters['settings/value'](this.settingName)
+			},
+			set: function(newSetting) {
+				this.$store.dispatch('settings/setToAccount', { key: this.settingName, value: newSetting })
+			}
+		},
 	},
 })
 </script>
