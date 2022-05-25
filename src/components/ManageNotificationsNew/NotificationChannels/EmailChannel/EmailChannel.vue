b7218e29559dc0e43cc62a5e282e7e4c736473dd
Michael Fuchs <michael@derfuchs.net>
2022-05-25 17:23:32 +0200

    Implemented email channel setup

--- src/components/ManageNotificationsNew/NotificationChannels/EmailChannel/EmailChannel.vue
+++ src/components/ManageNotificationsNew/NotificationChannels/EmailChannel/EmailChannel.vue
@@ -3,10 +3,17 @@
 		<q-item-section avatar top>
 			<q-icon name="fa-light fa-envelope" color="primary" />
 		</q-item-section>
+
 		<q-item-section>
 			<q-item-label>
-				E-Mail
-				<q-badge rounded color="warning" text-color="white">
+				Email
+				<q-badge v-if="channelActive" rounded color="positive" text-color="white">
+					<q-icon
+						name="fa-solid fa-circle-check"
+						size="14px"
+					/> &nbsp;active
+				</q-badge>
+				<q-badge v-else rounded color="warning" text-color="white">
 					<q-icon
 						name="fa-solid fa-circle-xmark"
 						size="14px"
@@ -15,18 +22,59 @@
 			</q-item-label>
 			<q-item-label caption>Born before the year 2000? Dobby got you covered.</q-item-label>
 		</q-item-section>
+
 		<q-item-section side top>
-			<q-btn flat dense rounded icon="fa-light fa-circle-plus" class="q-pt-none" />
+			<q-btn v-if="!channelActive" @click="showEmailChannelSetup = true" flat dense rounded icon="fa-light fa-circle-plus" class="q-pt-none" />
+			<ChannelOptionsMenu v-else icon="fa-light fa-envelope" icon-color="primary">
+				<TestChannel
+					:label="$t('Send Test Message')"
+					channel="mail"
+					:color="($q.dark.channelActive) ? 'primary' : 'accent'"
+					rounded
+					outline
+					class="full-width"
+				/>
+				<q-btn
+					:label="$t('Change Address')"
+					rounded
+					outline
+					class="full-width"
+					:color="($q.dark.channelActive) ? 'primary' : 'accent'"
+					@click="showEmailChannelSetup = true"
+				/>
+			</ChannelOptionsMenu>
 		</q-item-section>
 	</q-item>
+	<EmailChannelSetup v-model="showEmailChannelSetup" />
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import EmailChannelSetup from 'components/ManageNotificationsNew/NotificationChannels/EmailChannel/EmailChannelSetup.vue'
+import ChannelOptionsMenu from 'components/ManageNotificationsNew/NotificationChannels/ChannelOptionsMenu.vue'
+import TestChannel from 'components/ManageNotificationsNew/NotificationChannels/TestChannel.vue'
+
 export default {
 	name: 'EmailChannel',
 	components: {
-
+		EmailChannelSetup,
+		ChannelOptionsMenu,
+		TestChannel,
   },
+	data() {
+		return {
+			showEmailChannelSetup: false,
+		}
+	},
+	computed: {
+		channelActive: function() {
+			return this.hasGatewayType('mail')
+		},
+		...mapGetters({
+			hasGatewayType: 'notifications/hasGatewayType',
+		}),
+	}
 }
 </script>
 
---
+++ src/components/ManageNotificationsNew/NotificationChannels/EmailChannel/EmailChannelSetup.vue
@@ -0,0 +1,185 @@
+<template>
+	<q-dialog
+		persistent
+		maximized
+		transition-show="slide-up"
+		transition-hide="slide-down"
+	>
+		<q-card :class="{ 'bg-dark text-white': darkMode }">
+			<q-bar>
+				<q-space />
+
+				<q-btn dense flat icon="close" v-close-popup>
+					<q-tooltip class="bg-white text-primary">Close</q-tooltip>
+				</q-btn>
+			</q-bar>
+			<div class="q-pa-md">
+				<div class="text-h5 q-mb-md">
+					Get notified via Email <q-icon :name="emailIcon" color="primary" />
+				</div>
+				<p class="text-body1">
+					Dobby remembers the old days when email was the hot thing in the brand-new Internet.
+					If you've been born after the year 2000 you might not have such an emotional connection to this ancient stuff.
+					But hey, it's better than carrier pigeons.
+				</p>
+				<p>
+					Anyway, you can get notified via Email. Set it up here and you're good to go.
+				</p>
+
+				<p v-if="hasGatewayType('mail')">
+					You currently receive email notifications at <span class="text-h6 text-primary">{{ gatewayType('mail').value }}</span>
+				</p>
+
+				<p v-if="hasGatewayType('mail')">
+					You can change that email address to another one:
+				</p>
+
+				<p>
+					<q-input
+						type="email"
+						outlined
+						label="something like socks@defichain-dobby.com"
+						v-model="emailAddress"
+					/>
+					</p>
+					<p>
+					<q-input
+						type="email"
+						outlined
+						label="repeat email address"
+						v-model="emailAddressRepeat"
+					/>
+				</p>
+
+				<p>
+					<q-btn
+						unelevated
+						rounded
+						class="full-width"
+						:icon="emailIcon"
+						label="Set Email Address"
+						color="primary"
+						@click="setEmailAddress()"
+						:loading="loading"
+						:disable="emailAddress != emailAddressRepeat || emailAddress == null"
+					/>
+				</p>
+
+				<p v-if="done">
+					Done! You should have received an email.
+				</p>
+
+			</div>
+		</q-card>
+	</q-dialog>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import { openURL } from 'quasar'
+
+export default {
+	name: 'ConnectEmailChannel',
+	data() {
+		return {
+			sentTestMessage: false,
+			receivedTestMessage: null,
+			showContact: false,
+			emailIcon: 'fa-light fa-envelope',
+			emailAddress: null,
+			emailAddressRepeat: null,
+			loading: false,
+			done: false,
+		}
+	},
+	methods: {
+		isEmailAddress(string) {
+			return String(string)
+				.toLowerCase()
+				.match(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				)
+		},
+		setEmailAddress() {
+			// check if email address is valid
+			if (!this.isEmailAddress(this.emailAddress)) {
+				this.$q.notify({
+					group: 'userInputError',
+					type: 'negative',
+					message: 'This is not an email addess, sir',
+					timeout: 5000,
+				})
+				return
+			}
+			if (process.env.DEV) { console.log("[DEBUG] email address looks valid: " + this.emailAddress) }
+
+			this.loading = true
+
+			if ('gatewayId' in this.gatewayType('mail')) {
+				if (process.env.DEV) { console.log("[DEBUG] there's already an active email gateway: ID#" + this.gatewayType('mail').gatewayId + " | removing gateway") }
+				this.$api.delete('user/gateways', { data: { 'gateway_id': this.gatewayType('mail').gatewayId } })
+			}
+
+			// buzzjoe@gmail.com
+			// michael@derfuchs.net
+
+			// make API call to set new email notification gateway
+			this.$api
+				.post('user/gateways', {type: 'mail', value: this.emailAddress})
+				.then((result) => {
+					setTimeout(() => {
+						this.loading = false
+						this.done = true
+						this.testEmailChannel()
+						this.emailAddress = null
+						this.emailAddressRepeat = null
+						if (process.env.DEV) { console.log("[DEBUG] created new email gateway: " + this.emailAddress) }
+					}, 1500)
+					this.fetchGateways()
+				})
+				.catch((error) => {
+					this.$q.notify({
+						group: 'apiError',
+						type: 'negative',
+						message: 'Something went wrong',
+						timeout: 60000,
+					})
+				})
+		},
+
+		testEmailChannel() {
+			this.$api.post("/user/gateways/test", { 'type': 'mail' })
+				.then((result) => {
+					this.$q.notify({
+						group: 'gatewayTest',
+						type: 'positive',
+						message: 'Dobby should have said hello at ' + this.emailAddress + ' :)',
+					})
+				})
+		},
+
+		toTelegramGroup() {
+			openURL(process.env.TELEGRAM_GROUP_LINK)
+		},
+
+		...mapActions({
+			fetchGateways: 'notifications/fetchGateways',
+		})
+	},
+	computed: {
+		darkMode: function () {
+			return this.$q.dark.isActive
+		},
+		...mapGetters({
+			isDemo: 'account/isDemo',
+			userId: 'account/userId',
+			hasGatewayType: 'notifications/hasGatewayType',
+			gatewayType: 'notifications/gatewayType',
+		}),
+	}
+}
+</script>
+
+<style>
+
+</style>
