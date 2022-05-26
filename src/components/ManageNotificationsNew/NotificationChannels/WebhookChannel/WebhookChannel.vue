5815fe3478a5c033613f08317177eb301d9971a7
Michael Fuchs <michael@derfuchs.net>
2022-05-26 17:52:00 +0200

    Setup for webhook notification channel

--- src/components/ManageNotificationsNew/NotificationChannels/WebhookChannel/WebhookChannel.vue
+++ src/components/ManageNotificationsNew/NotificationChannels/WebhookChannel/WebhookChannel.vue
@@ -3,10 +3,17 @@
 		<q-item-section avatar top>
 			<q-icon name="fa-light fa-cloud" color="primary" />
 		</q-item-section>
+
 		<q-item-section>
 			<q-item-label>
-				Web Hook
-				<q-badge rounded color="warning" text-color="white">
+				Webhook
+				<q-badge v-if="channelActive" rounded color="positive" text-color="white">
+					<q-icon
+						name="fa-solid fa-circle-check"
+						size="14px"
+					/> &nbsp;active
+				</q-badge>
+				<q-badge v-else rounded color="warning" text-color="white">
 					<q-icon
 						name="fa-solid fa-circle-xmark"
 						size="14px"
@@ -15,18 +22,59 @@
 			</q-item-label>
 			<q-item-label caption>For the nerds</q-item-label>
 		</q-item-section>
+
 		<q-item-section side top>
-			<q-btn flat dense rounded icon="fa-light fa-circle-plus" class="q-pt-none" />
+			<q-btn v-if="!channelActive" @click="showWebhookChannelSetup = true" flat dense rounded icon="fa-light fa-circle-plus" class="q-pt-none" />
+			<ChannelOptionsMenu v-else icon="fa-light fa-cloud" icon-color="primary">
+				<TestChannel
+					:label="$t('Send Test Message')"
+					channel="webhook"
+					:color="($q.dark.channelActive) ? 'primary' : 'accent'"
+					rounded
+					outline
+					class="full-width"
+				/>
+				<q-btn
+					:label="$t('Change Address')"
+					rounded
+					outline
+					class="full-width"
+					:color="($q.dark.channelActive) ? 'primary' : 'accent'"
+					@click="showWebhookChannelSetup = true"
+				/>
+			</ChannelOptionsMenu>
 		</q-item-section>
 	</q-item>
+	<WebhookChannelSetup v-model="showWebhookChannelSetup" />
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import WebhookChannelSetup from 'components/ManageNotificationsNew/NotificationChannels/WebhookChannel/WebhookChannelSetup.vue'
+import ChannelOptionsMenu from 'components/ManageNotificationsNew/NotificationChannels/ChannelOptionsMenu.vue'
+import TestChannel from 'components/ManageNotificationsNew/NotificationChannels/TestChannel.vue'
+
 export default {
 	name: 'WebhookChannel',
 	components: {
-
+		WebhookChannelSetup,
+		ChannelOptionsMenu,
+		TestChannel,
   },
+	data() {
+		return {
+			showWebhookChannelSetup: false,
+		}
+	},
+	computed: {
+		channelActive: function() {
+			return this.hasGatewayType('webhook')
+		},
+		...mapGetters({
+			hasGatewayType: 'notifications/hasGatewayType',
+		}),
+	}
 }
 </script>
 
---
+++ src/components/ManageNotificationsNew/NotificationChannels/WebhookChannel/WebhookChannelSetup.vue
@@ -0,0 +1,169 @@
+<template>
+	<q-dialog
+		persistent
+		maximized
+		transition-show="slide-up"
+		transition-hide="slide-down"
+	>
+		<q-card :class="{ 'bg-dark text-white': darkMode }">
+			<q-bar>
+				<q-space />
+
+				<q-btn dense flat icon="close" v-close-popup>
+					<q-tooltip class="bg-white text-primary">Close</q-tooltip>
+				</q-btn>
+			</q-bar>
+			<div class="q-pa-md">
+				<div class="text-h5 q-mb-md">
+					Get notified via Webhook <q-icon :name="webhookIcon" color="primary" />
+				</div>
+				<p class="text-body1">
+					Dobby is able to push notifications to any URL you provide. This message contains a JSON object and all neccessary data.
+					If you have no clue what this means, you won't need a webhook notification and can ignore this notification channel.
+				</p>
+
+				<p v-if="hasGatewayType('webhook')">
+					You currently receive notifications at <span class="text-h6 text-primary">{{ gatewayType('webhook').value }}</span>
+				</p>
+
+				<p v-if="hasGatewayType('webhook')">
+					You can change that URL to another one:
+				</p>
+
+				<p>
+					<q-input
+						type="text"
+						outlined
+						label="Webhook URL"
+						v-model="webhookUrl"
+					/>
+				</p>
+
+				<p>
+					<q-btn
+						unelevated
+						rounded
+						class="full-width"
+						:icon="webhookIcon"
+						label="Set webhook URL"
+						color="primary"
+						@click="setwebhookUrl()"
+						:loading="loading"
+						:disable="webhookUrl == null"
+					/>
+				</p>
+
+				<p v-if="done">
+					Done! You should have received a test message.
+				</p>
+
+			</div>
+		</q-card>
+	</q-dialog>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import { openURL } from 'quasar'
+
+export default {
+	name: 'WebhookChannelSetup',
+	data() {
+		return {
+			sentTestMessage: false,
+			receivedTestMessage: null,
+			showContact: false,
+			webhookIcon: 'fa-light fa-cloud',
+			webhookUrl: null,
+			loading: false,
+			done: false,
+		}
+	},
+	methods: {
+		isUrl(string) {
+			return String(string)
+				.toLowerCase()
+				.match(
+					/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/
+				)
+		},
+		setwebhookUrl() {
+			// check if email address is valid
+			if (!this.isUrl(this.webhookUrl)) {
+				this.$q.notify({
+					group: 'userInputError',
+					type: 'negative',
+					message: 'This is not an URL, sir',
+					timeout: 5000,
+				})
+				return
+			}
+			if (process.env.DEV) { console.log("[DEBUG] URL looks valid: " + this.webhookUrl) }
+
+			this.loading = true
+
+			if (typeof this.gatewayType('webhook') == 'object' && 'gatewayId' in this.gatewayType('webhook')) {
+				if (process.env.DEV) { console.log("[DEBUG] there's already an active webhook gateway: ID#" + this.gatewayType('webhook').gatewayId + " | removing gateway") }
+				this.$api.delete('user/gateways', { data: { 'gateway_id': this.gatewayType('webhook').gatewayId } })
+			}
+
+			// make API call to set new email notification gateway
+			this.$api
+				.post('user/gateways', {type: 'webhook', value: this.webhookUrl})
+				.then((result) => {
+					setTimeout(() => {
+						this.loading = false
+						this.done = true
+						this.testEmailChannel()
+						this.webhookUrl = null
+						this.webhookUrlRepeat = null
+						if (process.env.DEV) { console.log("[DEBUG] created new webhook gateway: " + this.webhookUrl) }
+					}, 1500)
+					this.fetchGateways()
+				})
+				.catch((error) => {
+					this.$q.notify({
+						group: 'apiError',
+						type: 'negative',
+						message: 'Something went wrong',
+						timeout: 60000,
+					})
+				})
+		},
+
+		testEmailChannel() {
+			this.$api.post("/user/gateways/test", { 'type': 'webhook' })
+				.then((result) => {
+					this.$q.notify({
+						group: 'gatewayTest',
+						type: 'positive',
+						message: 'Dobby should have said hello at ' + this.gatewayType('webhook').value + ' :)',
+					})
+				})
+		},
+
+		toTelegramGroup() {
+			openURL(process.env.TELEGRAM_GROUP_LINK)
+		},
+
+		...mapActions({
+			fetchGateways: 'notifications/fetchGateways',
+		})
+	},
+	computed: {
+		darkMode: function () {
+			return this.$q.dark.isActive
+		},
+		...mapGetters({
+			isDemo: 'account/isDemo',
+			userId: 'account/userId',
+			hasGatewayType: 'notifications/hasGatewayType',
+			gatewayType: 'notifications/gatewayType',
+		}),
+	}
+}
+</script>
+
+<style>
+
+</style>
